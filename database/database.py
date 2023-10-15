6f179bcf47e91c7afbbf4e1ed8e9179fe2a820e7
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-10-15 16:44:25 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,12 +1,12 @@
 import os
 import redis
 
-REDIS_HOST = os.environ.get("REDIS_HOST", "redis-19153.c10.us-east-1-2.ec2.cloud.redislabs.com:19153")
+REDIS_HOST = os.environ.get("REDIS_HOST", "redis-19153.c10.us-east-1-2.ec2.cloud.redislabs.com")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 19153))
-REDIS_DB = int(os.environ.get("REDIS_DB", "f9Hstc2xMAI2FMGuLbGsn446LwsTM4c0"))
+REDIS_PASS = os.environ.get("REDIS_PASS", "")  # Redis password
 
 # Connect to Redis
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASS)
 
 async def present_user(user_id: int):
     return redis_client.exists(user_id)
